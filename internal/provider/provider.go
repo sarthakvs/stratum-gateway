8742ac28329f3852569908371a88b7b175215e29
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2026-09-15 19:45:38 +0530

    Stage1:Provider interface

---
+++ internal/provider/provider.go
@@ -0,0 +1,12 @@
+package provider
+import "context"
+
+type ChunkStream interface {
+	Next() (Chunk, error)
+	Close() error
+}
+
+type Provider interface {
+	Name() string
+	Stream(ctx context.Context,req Request) (ChunkStream,error)
+} 
